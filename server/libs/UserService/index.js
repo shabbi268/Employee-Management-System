eebbc6f3be20a8349a400dd67bcb14b3edeb5dc0
Kesa Shabarish <shabarish.shabbi@gmail.com>
2021-04-12 19:40:03 -0400

    removed server files

--- server/libs/UserService/index.js
+++
@@ -1,2 +0,0 @@
-let request = require(`request`);
-const config = require(`../Config`);
--- server/utils/index.js
+++
@@ -1,7 +0,0 @@
-const ErrorHandler = require(`./ErrorHandler`);
-const IndexRoute = require(`./IndexRoute`);
-const RouteLoader = require(`./RouteLoader`);
-
-exports.ErrorHandler = ErrorHandler;
-exports.IndexRoute = IndexRoute;
-exports.RouteLoader = RouteLoader;
